e20f748d5214047fabee397dd7e761a7d2eac865
Said Berk <csaidberk@gmail.com>
2023-03-05 15:18:19 +0300

    Main Logo of Home placed, debug banner made invisible.

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,8 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.white,
           appBarTheme:
               const AppBarTheme(backgroundColor: Colors.white, elevation: 0)),
-      home: const MyHomePage(),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
 
   final String title = "Ana Sayfa";
   final logo = 'assets/svg/logo-atcint.svg';
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -15,8 +18,15 @@ class MyHomePage extends StatelessWidget {
       body: SafeArea(
           child: Column(
         children: [
-          SvgPicture.asset(
-            logo,
+          Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: screenHeight / 5,
+              width: screenWidth / 1.2,
+              child: SvgPicture.asset(
+                logo,
+              ),
+            ),
           )
         ],
       )),
