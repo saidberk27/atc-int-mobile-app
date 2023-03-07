097d6445051283793d31c138741df077ebba13bf
Said Berk <csaidberk@gmail.com>
2023-03-07 14:25:14 +0300

    Firebase core installation completed.

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,14 @@ import 'package:atc_international/screens/refrigerations/refrigerations.dart';
 import 'package:flutter/material.dart';
 import 'local_components/colors.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
